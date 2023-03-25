28b3d0e36727b81ebdd288e797dc4c65e6be900b
Antonín Kadrmas <kadrmas.antonin@gmail.com>
2023-03-25 08:15:41 -0700

    retur to previous state

--- src/middleware/userAuthMiddlewareStrict.ts
+++ src/middleware/userAuthMiddlewareStrict.ts
@@ -5,7 +5,7 @@ import { TokenService } from '../service/tokenService';
 export async function userAuthMiddlewareStrict(request: express.Request, response: express.Response, next: NextFunction) {
     try{
         //@ts-ignore
-        const token = request.get("Authorization").token
+        const token = request.get("Authorization")
         console.log(token)
         if(token==null)throw Error("Application error.")//token does not exists in header
         const tokenService:TokenService = await TokenService.getInstance()
